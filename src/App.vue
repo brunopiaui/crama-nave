717708fe1f805a232916dd1021a631bcff95c7e6
Bruno Piaui <brunopiaui@gmail.com>
2019-11-30 22:23:04 +0000

    Iniciando testes com a logo

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   margin-top: 60px;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   color: #2c3e50;
   text-align: center;
   -webkit-font-smoothing: antialiased;
