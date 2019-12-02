34c31c241bde9b82b3250f399c0a5d9f1615830c
Paulo Victor Alcantara Martins Martins <victorrjtijuca@gmail.com>
2019-12-02 11:33:51 -0300

    pv teste

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
+      <p>pv teste</p>
     </div>
     <router-view />
   </div>
@@ -11,7 +12,7 @@
 <style>
 #app {
   margin-top: 60px;
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   color: #2c3e50;
   text-align: center;
   -webkit-font-smoothing: antialiased;
