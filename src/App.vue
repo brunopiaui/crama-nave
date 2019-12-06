e3ce9ec05ba32d87014845365b0202eec80dc23d
Bruno Piaui <brunopiaui@gmail.com>
2019-12-06 17:17:12 +0000

    Alteração na troca de página

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
       <span @click="routerTo(3)">Image03</span>
     </div>
     <transition name="router-anim" leave-to-class="animated routerZoom">
-      <router-view />
+      <component :is="componentLoader"></component>
     </transition>
   </div>
 </template>
@@ -15,6 +15,37 @@
 <script>
 export default {
   name: 'App',
+  components: {
+    Home: () =>
+      import(
+        /* webpackChunkName: "Home" */
+        /* webpackPreload: true */
+        '@/views/Home.vue'
+      ),
+    Image01: () =>
+      import(
+        /* webpackChunkName: "Image01" */
+        /* webpackPreload: true */
+        '@/views/Image01.vue'
+      ),
+    Image02: () =>
+      import(
+        /* webpackChunkName: "Image02" */
+        /* webpackPreload: true */
+        '@/views/Image02.vue'
+      ),
+    Image03: () =>
+      import(
+        /* webpackChunkName: "Image03" */
+        /* webpackPreload: true */
+        '@/views/Image03.vue'
+      ),
+  },
+  data() {
+    return {
+      componentLoader: 'Home',
+    }
+  },
   methods: {
     routerTo(index) {
       let app = document.getElementById('app')
@@ -23,14 +54,17 @@ export default {
       switch (index) {
         case 1:
           image = `url(${require('@/assets/images/img01.jpg')})`
+          this.componentLoader = 'Image01'
           next = 'image01'
           break
         case 2:
           image = `url(${require('@/assets/images/img02.jpg')})`
+          this.componentLoader = 'Image02'
           next = 'image02'
           break
         case 3:
           image = `url(${require('@/assets/images/img03.jpg')})`
+          this.componentLoader = 'Image03'
           next = 'image03'
           break
       }
