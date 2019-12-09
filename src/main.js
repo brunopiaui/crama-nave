8d470cc92a15f72694a0afd4dba5ccc89583a9ba
Bruno Piaui <brunopiaui@gmail.com>
2019-12-09 11:46:57 +0000

    Retirada da barra de progresso

--- src/main.js
+++ src/main.js
@@ -3,30 +3,13 @@ import App from './App.vue'
 import router from './router'
 import Vuex from 'vuex'
 import { store } from './store'
-
 import gsap from 'gsap'
-
-import NProgress from 'nprogress'
-import '../node_modules/nprogress/nprogress.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuex)
 
 Vue.prototype.$gsap = gsap
-
-NProgress.configure({ showSpinner: false })
-
-router.beforeResolve((to, from, next) => {
-  if (to.name) {
-    NProgress.start()
-  }
-  next()
-})
-
-router.afterEach((to, from) => {
-  NProgress.done()
-})
 
 new Vue({
   router,
